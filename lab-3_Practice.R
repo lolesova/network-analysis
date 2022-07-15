68097e3bb212a386e248ba5778941f2c9481b376
Larisa Olesova <lolesova@gmail.com>
2022-07-15 09:28:27 -0400

    network update
    network update

---
+++ lab-3_Practice.R
@@ -0,0 +1,128 @@
+install.packages("readxl")
+library(readxl)
+library(tidygraph)
+library(ggraph)
+install.packages("tidygraph")
+library(tidygraph)
+install.packages("ggraph")
+library(ggraph)
+student_nodes <- read_excel("lab-1/data/student-attributes.xlsx")
+student_edges <- read_excel("lab-1/data/student-edgelist.xlsx")
+student_network <- tbl_graph(edges = student_edges,
+                             nodes = student_nodes, 
+                             directed = TRUE)
+student_network
+plot(student_network)
+autograph(student_network)
+ggraph(student_network)
+ggraph(student_network) + 
+  geom_node_point()
+  geom_edge_link()
+  ggraph(student_network) + 
+    geom_node_point() + 
+    geom_edge_fan()  
+  ggraph(student_network, layout = "stress") + 
+    geom_edge_link(arrow = arrow(length = unit(1, 'mm')), 
+                   end_cap = circle(3, 'mm'),
+                   start_cap = circle(3, 'mm'),
+                   alpha = .1) +
+    geom_node_point(aes(size = local_size(),
+                        color = gender)) +
+    geom_node_text(aes(label = id),
+                   repel=TRUE) +
+    theme_graph()
+  library(tidygraph)
+  library(ggraph)
+  library(tidyverse)
+  student_nodes <- read_excel("lab-1/data/student-attributes.xlsx")
+  student_edges <- read_excel("lab-1/data/student-edgelist.xlsx")
+  student_network <- tbl_graph(edges = student_edges,
+                               nodes = student_nodes, 
+                               directed = TRUE)  
+  student_network <- tbl_graph(edges = student_edges,
+                               nodes = student_nodes, 
+                               directed = TRUE)  
+plot(student_network)  
+autograph(student_network)
+ggraph(student_network, layout = "stress") + 
+  geom_edge_link(arrow = arrow(length = unit(1, 'mm')), 
+                 end_cap = circle(3, 'mm'),
+                 start_cap = circle(3, 'mm'),
+                 alpha = .1) +
+  geom_node_point(aes(size = local_size(),
+                      color = gender)) +
+  geom_node_text(aes(label = id),
+                 repel=TRUE) +
+  theme_graph()
+install.packages("janitor")
+library(janitor)
+library(tidyverse)
+library(tidygraph)
+library(ggraph)
+library(igraph)
+dlt1_ties <- read_csv("lab-2/data/dlt1-edges.csv", 
+                      col_types = cols(Sender = col_character(), 
+                                       Receiver = col_character(), 
+                                       `Category Text` = col_skip(), 
+                                       `Comment ID` = col_character(), 
+                                       `Discussion ID` = col_character())) |>
+  clean_names()
+dlt1_actors <- read_csv("lab-2/data/dlt1-nodes.csv", 
+                        col_types = cols(UID = col_character(), 
+                                         Facilitator = col_character(), 
+                                         expert = col_character(), 
+                                         connect = col_character())) |>
+  clean_names()
+dlt1_network <- tbl_graph(edges = dlt1_ties,
+                          nodes = dlt1_actors,
+                          node_key = "uid",
+                          directed = TRUE)
+dlt1_network
+plot(dlt1_network)
+autograph(dlt1_network)
+library(tidyverse)
+library(igraph)
+library(tidygraph)
+library(ggraph)
+ccss_ties <- read_csv("lab-3/data/ccss-edgelist.csv")
+ccss_nodes <- read_csv("lab-3/data/ccss-nodelist.csv")
+ccss_network <- tbl_graph(edges = ccss_ties,
+                          nodes = ccss_nodes, 
+                          directed = TRUE)
+ccss_network
+ggraph(ccss_network, layout = "fr") + 
+  geom_edge_link(arrow = arrow(length = unit(1, 'mm')), 
+                 end_cap = circle(3, 'mm'),
+                 start_cap = circle(3, 'mm'),
+                 alpha = .1) +
+  geom_node_point(aes(size = local_size())) +
+  geom_node_text(aes(label = actors,
+                     size = local_size()),
+                 repel=TRUE) +
+  theme_graph()
+autograph(ccss_network)
+ggraph(ccss_network, layout = "fr") + 
+  geom_edge_link(arrow = arrow(length = unit(1, 'mm')), 
+                 end_cap = circle(3, 'mm'),
+                 start_cap = circle(3, 'mm'),
+                 alpha = .1) +
+  geom_node_point(aes(size = local_size())) +
+  geom_node_text(aes(label = actors,
+                     size = local_size()),
+                 repel=TRUE) +
+  theme_graph()
+ccss_network
+components(ccss_network, mode = "strong")
+clique_num(ccss_network)
+cliques(ccss_network, min = 4, max = NULL)
+ccss_network <- ccss_network |>
+  activate(nodes) |>
+  mutate(in_degree = centrality_degree(mode = "in"),
+         out_degree = centrality_degree(mode = "out"))
+ccss_network
+
+degree_table <- ccss_network |>
+  activate(nodes) |>
+  mutate(in_degree = centrality_degree(mode = "in"),
+         out_degree = centrality_degree(mode = "out")) |>
+  as_tibble()
